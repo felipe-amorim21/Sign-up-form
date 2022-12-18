ec46603b09b283d16a5d9da1f880749119cfa051
Felipe <felipe21fab@gmail.com>
2022-12-18 14:07:45 -0300

    Finished check password match

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-get
+const pwd = document.getElementById('pwd')
+const cpwd = document.getElementById('cpwd')
+const pwderror = document.getElementsByClassName('pwderror')
+
+cpwd.addEventListener('input', () => {
+    if (pwd.value != cpwd.value){
+        pwd.classList.add('error')
+        cpwd.classList.add('error')
+        pwderror[0].textContent = "Passwords do not match"
+        pwderror[1].textContent = "Passwords do not match"
+    }
+    else {
+        pwd.classList.remove('error')
+        cpwd.classList.remove('error')
+        pwderror[0].textContent = ""
+        pwderror[1].textContent = ""
+    }
+})
